a4a3db6fb4755122aba255c86fd5118f4415a53b
seeyouerase <ddosss2312@yandex.ru>
2024-02-09 18:48:37 +0300

    Formatted

--- Laba1.java
+++ Laba1.java
@@ -1,4 +1,3 @@
-
 /*
  * Laba1
  */
@@ -10,10 +9,11 @@ public class Laba1 {
         }
         return sum;
     }
-	public static void main(String[] args) {
-        int[] A =new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-        int[] B =new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-        int[] C = new int[] {1,2,3,4,5,6,7,8,9,10};
+
+    public static void main(String[] args) {
+        int[] A = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        int[] B = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        int[] C = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int Asum = sum(A);
         int Bsum = sum(B);
         int Csum = sum(C);
